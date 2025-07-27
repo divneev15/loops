f807943aa6c4eaccb604a94fd4013f1d9f5e6e4d
divneev15 <divneevsingh123@gmail.com>
2025-07-27 16:29:41 +0530

    Create number_series.cpp

---
+++ number_series.cpp
@@ -0,0 +1,32 @@
+/* divneev singh
+entc A2
+PRN 24070123043 */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, num = 1;
+
+    for (i = 1; i <= 4; i++) 
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/* Output for this code
+
+1 
+2 3 
+4 5 6 
+7 8 9 10
+
+*/
