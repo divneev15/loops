d8a665e00d6ae49c416b9636b92ce7d80eb07f37
divneev15 <divneevsingh123@gmail.com>
2025-07-27 16:26:31 +0530

    Create starsimple_pattern.cpp

---
+++ starsimple_pattern.cpp
@@ -0,0 +1,32 @@
+/* Divneev singh 
+PRN 24070123043
+  entc A2 */
+
+
+// simple star pyramid
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    int n=5;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<"*";
+            
+        }
+        cout<<endl;
+    }
+}
+
+/* Output for this code
+
+*
+**
+***
+****
+*****
+
+*/
