54e835903444001d757e1b9b7b1da83073d4caab
divneev15 <divneevsingh123@gmail.com>
2025-07-27 16:18:03 +0530

    Create sit_5times.cpp

---
+++ sit_5times.cpp
@@ -0,0 +1,25 @@
+/* Divneev Singh
+PRN 24070123043
+ENTC A2 */
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1; i<6; i++)
+    {
+        cout<<"SIT\n";
+    }
+}
+
+/* Output for this code
+
+SIT
+SIT
+SIT
+SIT
+SIT
+
+*/
