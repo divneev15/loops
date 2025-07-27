3c1e29906e0f203ddb65ed66a58b48be49816234
divneev15 <divneevsingh123@gmail.com>
2025-07-27 16:40:01 +0530

    Create rev_simple_pyr.cpp

---
+++ rev_simple_pyr.cpp
@@ -0,0 +1,29 @@
+/* divneev
+24070123043 EntcA2
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 6;
+
+    for (int i = rows; i >= 1; --i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+/*
+output 
+******
+*****
+****
+***
+**
+*
+
+*/
